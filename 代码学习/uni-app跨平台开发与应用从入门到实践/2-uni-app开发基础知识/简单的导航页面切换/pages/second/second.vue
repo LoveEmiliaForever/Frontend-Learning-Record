39186724d7da28db4b3b36d6ccfbea6e587f97b4
LoveEmiliaForever <913106458@qq.com>
2023-10-20 23:37:41 +0800

    学习了《uniapp跨平台开发与应用从入门到实践》的1、2、3章

---
+++ 代码学习/uni-app跨平台开发与应用从入门到实践/2-uni-app开发基础知识/简单的导航页面切换/pages/second/second.vue
@@ -0,0 +1,24 @@
+<template>
+    <view>
+        <navigator url="../index/index" open-type="switchTab">
+            <button type="primary">回到首页</button>
+        </navigator>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 代码学习/uni-app跨平台开发与应用从入门到实践/3-uni-app中Vue的使用/使用uni-app内置表单组件/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+        <picker @change="bindPickerChange" :value="index" :range="array">
+            <view class="picker">
+                当前性别: {{array[index]}}
+            </view>
+        </picker>
+        <radio-group class="radio-group" @change="radioChange">
+            <label class="radio" v-for="(item, index) in items" :key="item.name">
+                <radio :value="item.name" :checked="item.checked"></radio>
+                {{item.value}}
+            </label>
+        </radio-group>
+        当前性别: {{index2}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+                index: 0,
+                array: ['男', '女', '其它'],
+                index2: 'BOY',
+                items: [
+                    {name:'BOY', value:'男', checked:'true'},
+                    {name:'GIRL', value:'女'},
+                    {name:'OTHER', value:'其它'}
+                ]
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+            bindPickerChange (e) {
+                this.index = e.detail.value
+            },
+            radioChange (e) {
+                this.index2 = e.detail.value
+            }
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
